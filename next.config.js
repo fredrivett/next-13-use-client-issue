9cc3532626f1d46afa927da9a3064e1aa473bbc7
Fred Rivett <fred@fredrivett.com>
2023-02-27 21:30:34 +0000

    reproduce `"use client"` error using `withTwin`
    the build process is stripping out the `"use client"` in
    `/src/lib/registry.ts`, causing this error:
    
    ```
    You're importing a component that needs useState. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
    
       ,-[/Users/fredrivett/code/FR/next-13-use-client-issue/src/lib/registry.tsx:1:1]
     1 | import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
     2 | import React, { useState } from "react";
       :                 ^^^^^^^^
     3 | import { useServerInsertedHTML } from "next/navigation";
     4 | import { ServerStyleSheet, StyleSheetManager } from "styled-components";
     5 | export default function StyledComponentsRegistry({ children  }) {
       `----
    ```

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = nextConfig
+const withTwin = require("./withTwin.js");
+
+const baseUrl = "";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withTwin(
+  withBundleAnalyzer({
+    reactStrictMode: true, // < Recommended by Next
+    poweredByHeader: false,
+    trailingSlash: true,
+    basePath: baseUrl,
+    env: {
+      baseUrl,
+    },
+    // The starter code load resources from `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    images: {
+      // fixes build error (see commit message for details)
+      unoptimized: true,
+    },
+    experimental: {
+      appDir: true,
+    },
+  })
+);
